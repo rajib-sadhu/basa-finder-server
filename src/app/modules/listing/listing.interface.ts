916cbb5a552da2565e5293f9f7f50496797ab3a1
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2025-04-20 05:05:50 +0530

    some changes in listing

--- src/app/modules/listing/listing.interface.ts
+++ src/app/modules/listing/listing.interface.ts
@@ -1,14 +1,14 @@
 import { Types } from "mongoose";
 
 export interface IListing {
-	title: string;
-	location: string;
-	description: string;
-	rent: number;
-	bedrooms: number;
-	images: string[];
-	landlordId: Types.ObjectId; // user ID reference
-	createdAt?: Date;
-	updatedAt?: Date;
-  }
-  
+  title: string;
+  location: string;
+  description: string;
+  rent: number;
+  bedrooms: number;
+  images: string[];
+  landlordId: Types.ObjectId; // user ID reference
+  createdAt?: Date;
+  updatedAt?: Date;
+  amenities?: string[];
+}
