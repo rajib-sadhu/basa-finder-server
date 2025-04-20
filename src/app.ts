6f78a315687a9489ae2e1c5d090eb0f2f34e238a
e <jannatul0040@gmail.com>
2025-04-20 08:42:35 +0600

    Added get user route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
 
-import { globalErrorHandler } from './app/middleeatres/globalErrorHandler';
-import userRouter from './app/modules/user/user.router';
-import listingRouter from './app/modules/listing/listing.router';
-import requestRouter from './app/modules/request/request.router';
-import authRouter from './app/modules/auth/auth.router';
-
+import { globalErrorHandler } from "./app/middleeatres/globalErrorHandler";
+import userRouter from "./app/modules/user/user.router";
+import listingRouter from "./app/modules/listing/listing.router";
+import requestRouter from "./app/modules/request/request.router";
+import authRouter from "./app/modules/auth/auth.router";
 
 const app: Application = express();
 
@@ -17,6 +16,8 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "http://localhost:3000",
+      "https://basa-finder-psi.vercel.app",
     ],
     credentials: true,
   })
@@ -27,21 +28,21 @@ app.use(express.json());
 
 // application routes
 const getAController = (req: Request, res: Response) => {
-    res.send('Hello World!')
+  res.send("Hello World!");
 };
-app.get('/', getAController)
+app.get("/", getAController);
 
 // routes
-app.use('/api/auth', authRouter);
-app.use('api/users', userRouter);
-app.use('api/listings', listingRouter);
-app.use('api/requests', requestRouter);
-app.use(globalErrorHandler)
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/listings", listingRouter);
+app.use("/api/requests", requestRouter);
+app.use(globalErrorHandler);
 
-app.use("*", (req: Request, res: Response) =>{
+app.use("*", (req: Request, res: Response) => {
   res.status(404).json({
     status: false,
-    message: "Route not found"
-  })
-})
+    message: "Route not found",
+  });
+});
 export default app;
--- src/app/modules/user/user.router.ts
+++ src/app/modules/user/user.router.ts
@@ -1,14 +1,18 @@
 import { NextFunction, Request, Response, Router } from "express";
- import { userController } from "./user.controller";
- import { UserValidation } from "./userValidation";
- import { USER_ROLE } from "./user.constants";
- import validateRequest from './../../middleeatres/validateRequest';
- 
- const userRouter = Router()
- userRouter.post('/create-admin', validateRequest(UserValidation.userValidationSchema),
-     userController.createAdmin)
- userRouter.get('/:userId', userController.getSingleUser)
- userRouter.put('/:userId', userController.updateUser)
- userRouter.delete('/:userId', userController.deleteUser)
- 
- export default userRouter
+import { userController } from "./user.controller";
+import { UserValidation } from "./userValidation";
+import { USER_ROLE } from "./user.constants";
+import validateRequest from "./../../middleeatres/validateRequest";
+
+const userRouter = Router();
+userRouter.post(
+  "/create-admin",
+  validateRequest(UserValidation.userValidationSchema),
+  userController.createAdmin
+);
+userRouter.get("/", userController.getUser);
+userRouter.get("/:userId", userController.getSingleUser);
+userRouter.put("/:userId", userController.updateUser);
+userRouter.delete("/:userId", userController.deleteUser);
+
+export default userRouter;
